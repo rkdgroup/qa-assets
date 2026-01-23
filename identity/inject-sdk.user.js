84af9408da74f911bf1c04a2cc74b66e209914cd
just Alan. <itzjustalan@gmail.com>
2026-01-23 18:42:37 +0530

    Update UserScript metadata for new namespace and URLs

--- identity/inject-sdk.user.js
+++ identity/inject-sdk.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Inject Identity SDK (QA)
-// @namespace    https://gist.githubusercontent.com/itzjustalan/gists
+// @namespace    https://gist.githubusercontent.com/rkdgroup/qa-assets
 // @version      0.0.1
 // @description  Injects latest identity SDK into page head for QA testing
 // @author       Alan
 // @match        https://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
-// @updateURL    https://gist.github.com/itzjustalan/2098735c640f1ce05b0f7a3242d3e9c9/raw/6dee043e3bd682fa2d9c96bebefcd6898984bd28/inject-identity-sdk.user.js
-// @downloadURL  https://gist.github.com/itzjustalan/2098735c640f1ce05b0f7a3242d3e9c9/raw/6dee043e3bd682fa2d9c96bebefcd6898984bd28/inject-identity-sdk.user.js
+// @updateURL    https://github.com/rkdgroup/qa-assets/raw/refs/heads/itzjustalan-patch-1/identity/inject-sdk.user.js
+// @downloadURL  https://github.com/rkdgroup/qa-assets/raw/refs/heads/itzjustalan-patch-1/identity/inject-sdk.user.js
 // ==/UserScript==
 
 (function() {
